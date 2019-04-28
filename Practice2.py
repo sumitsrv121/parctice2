d29d20b5b5ff2f029e61e9ec01743a4dd038cb2d
Sumit <sumitsaurav121@gmail.com>
2019-04-28 12:08:18 +0530

    New files added

---
+++ Practice2.py
@@ -0,0 +1,18 @@
+user = {
+    'name': 'sumit',
+    'age': 22,
+    'fav_movies': ['3 idiots','Tare zameen par','Jab we met'],
+    'mobile_phone': 'Redmi Note 7 pro',
+
+    'user1': {
+        'name': 'sumit',
+        'age': 22,
+        'fav_movies': ['3 idiots','Jab we met'],
+        'mobile_phone': 'Redmi Note 3'
+    }
+}
+
+# print(user)
+
+print(user['user1']['mobile_phone'])
+print(user['user1']['fav_movies'])
